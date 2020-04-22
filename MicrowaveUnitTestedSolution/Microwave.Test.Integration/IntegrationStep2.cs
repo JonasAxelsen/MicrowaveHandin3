5b1d131615cc0d6a17d6eb345630343009ec9943
Sebastian <sebastian@nielsen2u.dk>
2020-04-22 16:00:33 +0200

    override to avoid merge error in step 2

--- MicrowaveUnitTestedSolution/Microwave.Test.Integration/IntegrationStep2.cs
+++ MicrowaveUnitTestedSolution/Microwave.Test.Integration/IntegrationStep2.cs
@@ -14,31 +14,7 @@ namespace Microwave.Test.Integration
     [TestFixture]
     public class IntegrationStep2
     {
-        [TestFixture]
-        public class IT2_DisplayPowertube
-        {
-            private Display _display;
-            private PowerTube _powerTube;
-            private IOutput _output;
-            private ITimer _timer;
-            private CookController _cookController;
-
-            [SetUp]
-            public void SetUp()
-            {
-                _timer = Substitute.For<ITimer>();
-                _output = Substitute.For<IOutput>();
-                _display = new Display(_output);
-                _powerTube = new PowerTube(_output);
-                _cookController = new CookController(_timer, _display, _powerTube);
-            }
-            [Test]
-            public void Dosomething()
-            {
-                _timer.Start(2);
-                _cookController.StartCooking();
-            }
-        }
+        
         [Test]
         public void TestTest()
         {
