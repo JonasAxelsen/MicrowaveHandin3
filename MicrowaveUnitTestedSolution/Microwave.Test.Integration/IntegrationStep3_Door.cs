517604eab1bf5b12748b6955ceb65087ad229b04
Anders Tøgersen <54847232+au563513@users.noreply.github.com>
2020-04-27 12:01:14 +0200

    Integrationstep 3, mangler negative tests

--- MicrowaveUnitTestedSolution/Microwave.Test.Integration/IntegrationStep3_Door.cs
+++ MicrowaveUnitTestedSolution/Microwave.Test.Integration/IntegrationStep3_Door.cs
@@ -27,64 +27,175 @@ namespace Microwave.Test.Integration
         private Display _display;
         private IOutput _output;
         private ICookController _cookController;
-        private IButton _button;
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _startCancelButton;
 
         [SetUp]
         public void Setup()
         {
             _output = Substitute.For<IOutput>();
-            _button = Substitute.For<IButton>();
+            _powerButton = Substitute.For<IButton>();
+            _timeButton = Substitute.For<IButton>();
+            _startCancelButton = Substitute.For<IButton>();
             _cookController = Substitute.For<ICookController>();
 
             _door = new Door();
             _light = new Light(_output);
             _display = new Display(_output);
-            _userInterface = new UserInterface(_button,_button,_button, _door, _display, _light, _cookController);
+            _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
         }
 
+        #region Test throws
+
         [Test]
-        public void Door_OpenOnce_NoThrow()
+        public void UserInterfaceReadyState_Open_NoThrow()
         {
             Assert.That(() => _door.Open(), Throws.Nothing);
         }
 
         [Test]
-        public void Door_OpenTwice_NoThrow()
+        public void UserInterfaceOpenState_Open_NoThrow()
         {
             _door.Open();
             Assert.That(() => _door.Open(), Throws.Nothing);
-        }        
-        
+        }
+
         [Test]
-        public void Door_Close_NoThrow()
+        public void UserInterfacePowerState_Open_NoThrow()
+        {
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            Assert.That(() => _door.Open(), Throws.Nothing);
+        }
+
+        [Test]
+        public void UserInterfaceTimeState_Open_NoThrow()
+        {
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            Assert.That(() => _door.Open(), Throws.Nothing);
+        }
+
+        [Test]
+        public void UserInterfaceCookingState_Open_NoThrow()
+        {
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            Assert.That(() => _door.Open(), Throws.Nothing);
+        }
+
+        [Test]
+        public void UserInterfaceReadyState_Close_NoThrow() 
         {
             Assert.That(() => _door.Close(), Throws.Nothing);
         }
 
         [Test]
-        public void Door_Open_OutputRecievesCorrektString()
+        public void UserInterfaceOpenState_Close_NoThrow()
         {
             _door.Open();
 
-            _output.Received(1).OutputLine(Arg.Is<string>(str=>str.Contains("Light is turned on")));
-        }        
-        
-        [Test]
-        public void Door_Close_OutputRecievesNoCall()
-        {
-            _door.Close();
+            Assert.That(() => _door.Close(), Throws.Nothing);
+        }
 
-            _output.DidNotReceive().OutputLine(Arg.Any<string>());
-        }        
-        
         [Test]
-        public void Door_OpenClose_OutputRecievesNoCall()
+        public void UserInterfacePowerState_Close_NoThrow()
+        {
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            Assert.That(() => _door.Close(), Throws.Nothing);
+        }
+
+        [Test]
+        public void UserInterfaceTimeState_Close_NoThrow()
+        {
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            Assert.That(() => _door.Close(), Throws.Nothing);
+        }
+
+        [Test]
+        public void UserInterfaceCookingState_Close_NoThrow()
+        {
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            Assert.That(() => _door.Close(), Throws.Nothing);
+        }
+
+        #endregion
+
+        #region Test for right output
+
+        [Test]
+        public void UserInterfaceReadyState_Open_OutputRecievesCorrektString()
+        {
+            _door.Open();
+
+            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("Light is turned on")));
+        }
+
+        [Test]
+        public void UserInterfacePowerState_Open_OutputRecievesCorrektString()
+        {
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            _door.Open();
+
+            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("Display cleared")));
+            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("Light is turned on")));
+        }
+
+        [Test]
+        public void UserInterfaceTimeState_Open_OutputRecievesCorrektString()
+        {
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            _door.Open();
+
+            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("Display cleared")));
+            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("Light is turned on")));
+        }
+
+        [Test]
+        public void UserInterfaceCookingState_Open_OutputRecievesCorrektString()
+        {
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            _door.Open();
+
+            _cookController.Received(1).Stop();
+            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("Display cleared"))); //Lavede fejl i koden
+        }
+
+        [Test]
+        public void UserInterfaceOpenState_OpenClose_OutputRecievesCorrektString()
         {
             _door.Open();
             _door.Close();
 
             _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("Light is turned off")));
         }
-        
+
+        [Test]
+        public void Door_Close_OutputRecievesNoCall()
+        {
+            _door.Close();
+
+            _output.DidNotReceive().OutputLine(Arg.Any<string>());
+        }
+
+
+        #endregion
+
     }
 }
